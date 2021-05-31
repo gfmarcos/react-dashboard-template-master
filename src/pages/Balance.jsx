69737c010fdd9ac50df9edb43ae46553c2050b03
dididothat <alems.info@gmail.com>
2021-05-31 10:30:42 +0200

    update#2 session call

--- src/pages/Balance.jsx
+++ src/pages/Balance.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 import { movementsMapper } from "../data/movementsMapper";
 
-// const data = movementsMapper().map((movement) => {
-//     [
-//         {
-//             name: movement.transaction, value: movement.accountPayment ? movement.amount: -movement.amount
-//         },
-//     ]
-// }),
 
-export const balance = () => {
 
-    //console.log(data)
+export const Balance = () => {
 
+     const datas = movementsMapper().map((movement) => (
+         [
+             {
+                 name: movement.transaction, value: movement.accountPayment ? movement.amount: -movement.amount
+             },
+         ]
+     ));
+
+     console.log(data)
 
     return (
         <div>
-            <AreaChart width={730} height={250} data={null}
+            <AreaChart width={730} height={250} data=
+            {datas.map}
                 margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                 <defs>
                 <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
