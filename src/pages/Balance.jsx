a3d1b9de822c0cf7fe771523fa3a551cb0b2064b
dididothat <alems.info@gmail.com>
2021-05-31 11:34:57 +0200

    otro update

--- src/pages/Balance.jsx
+++ src/pages/Balance.jsx
@@ -6,20 +6,22 @@ import { movementsMapper } from "../data/movementsMapper";
 
 export const Balance = () => {
 
-     const datas = movementsMapper().map((movement) => (
-         [
-             {
-                 name: movement.transaction, value: movement.accountPayment ? movement.amount: -movement.amount
-             },
-         ]
-     ));
+    const datas_1 = movementsMapper().map((movement) => (
+        {
+            name: movement.transaction, value: movement.accountPayment ? movement.amount: -movement.amount
+        }
+             
+    ));
 
-     console.log(data)
-
+    const datas_2 = movementsMapper().map((movement) => (
+        {
+            name: movement.transaction, value: movement.accountPayment ? movement.amount: -movement.amount
+        }
+             
+    ));
     return (
         <div>
-            <AreaChart width={730} height={250} data=
-            {datas.map}
+            <AreaChart width={730} height={250} data={datas_1}
                 margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                 <defs>
                 <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -27,11 +29,11 @@ export const Balance = () => {
                     <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
                 </linearGradient>
             </defs>
-            <XAxis dataKey="name" />
+            <XAxis dataKey='name' />
             <YAxis />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
-            <Area type="monotone" dataKey="value" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+            <Area type="monotone" dataKey='value' stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
             </AreaChart>
             <div>
             {movementsMapper().map((movement) => (
